ac9858b1a335fe67243fef32c0681801ed704a47
Łukasz Merynda <lmerynda@gmail.com>
2016-01-04 17:34:22 +0100

    queue creation on server done

--- project5/server.c
+++ project5/server.c
@@ -10,8 +10,53 @@
 #include <fcntl.h>
 #include <errno.h>
 
+key_t key;
+int queueId;
+
+void initializeGlobals();
+void createMessageQueue();
+void deleteMessageQueue();
+
+void initializeGlobals()
+{
+  key = ftok("server.out",10);
+}
+
+void createMessageQueue()
+{
+  queueId = msgget(key, 0600|IPC_CREAT|IPC_EXCL);
+  if(queueId == -1)
+  {
+    printf("Blad tworzenia kolejki, moze istniec kolejka o kluczu: %d\n",key);
+    exit(1);
+  }
+  
+  printf("Utworzona kolejka o id: %d\n",queueId);
+}
+
+void deleteMessageQueue()
+{
+
+  if(msgctl(queueId, IPC_RMID, NULL) == -1)
+  // awakening all waiting reader and writer processes (with an error return and errno set to EIDRM
+  {
+    printf("Blad usuwania kolejki o id: %d\n",queueId);
+    exit(1);
+  }
+
+  printf("Usunieta kolejka o id: %d\n",queueId);
+}
+
+
 int main()
 {
+  initializeGlobals();
+
+  createMessageQueue();
+
+  sleep(10);
+
+  deleteMessageQueue();
 
   return 0;
 }
