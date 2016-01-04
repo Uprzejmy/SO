254a2a3b43e0145030cba56a30844a9f1b5d9d12
Łukasz Merynda <lmerynda@gmail.com>
2016-01-04 17:03:29 +0100

    project5 init

---
+++ project5/server.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/msg.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main()
+{
+
+  return 0;
+}
