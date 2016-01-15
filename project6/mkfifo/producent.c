e3f2a3e9654dc9b857195f7d7152915e1c771044
Łukasz Merynda <lmerynda@gmail.com>
2016-01-16 00:45:19 +0100

    creating fifo file

--- project6/mkfifo/producent.c
+++ project6/mkfifo/producent.c
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
+
+char fifoFilename[] = "transfer";
 
 void generateFile()
 {
@@ -31,18 +34,53 @@ void generateFile()
   fclose(fop);
 }
 
+void makeFifo()
+{
+  if(mkfifo(fifoFilename, 0600)==-1)
+  {
+    if(errno == EEXIST)
+    {
+      printf("Fifo juz istnieje, usuwan i tworze nowy\n");
+      unlink(fifoFilename);
+      makeFifo();
+    }
+    else
+    {
+      //dorzucic errno
+      printf("Blad tworzenia fifo\n");
+    }
+    
+  }
+  else
+  {
+    printf("Stworzono fifo\n");
+  }
+}
+
 void initialize()
 {
   srand(time(NULL));
 
   generateFile();
+
+  makeFifo();
+}
+
+void cleanup()
+{
+
+}
+
+void sendData()
+{
+
 }
 
 int main()
 {
   initialize();
   
-  
+  //sendData();
 
   cleanup();
 
